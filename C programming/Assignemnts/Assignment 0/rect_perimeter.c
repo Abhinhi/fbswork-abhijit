1993096015a7bf86f2b5e2c5f0f1beb3a2bbc3cc
Abhijit <abhijitbiradar7058@gmail.com>
2026-06-12 08:48:56 +0530

    Delete C programming/Assignemnts/Assignment 0 directory

--- C programming/Assignemnts/Assignment 0/rect_perimeter.c
+++
@@ -1,6 +0,0 @@
-void main(){
-	int length = 40;
-	int width = 25;
-	int perimeter = 2*(length + width);
-	printf("rectangle perimeter is:%d",perimeter);
-}
--- C programming/Assignemnts/Assignment 0/twonumadd.c
+++
@@ -1,6 +0,0 @@
-void main(){
-	int num1 = 10;
-	int num2 = 25;
-	int sum = num1+num2;
-	printf("%d",sum);
-}
