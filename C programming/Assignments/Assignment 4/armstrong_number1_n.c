8b5e94a889fa9545722649ba99887a1afca684af
Abhijit <abhijitbiradar7058@gmail.com>
2026-06-15 12:04:08 +0530

    Add files via upload

---
+++ C programming/Assignments/Assignment 4/armstrong_number1_n.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main(){
+	int n;
+	printf("enter number");
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++){
+	
+	int rem,temp,sum=0;
+	temp=i;
+	while(temp>0){
+		rem=temp%10;
+		sum=sum+rem*rem*rem;
+		temp=temp/10;
+	}
+	if(i==sum)
+	printf("armstrong numbers are%d\n",i);
+	
+	}
+}
